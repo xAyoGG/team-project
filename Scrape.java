68b8ed555b6708af7baee36dce3a15ab958c3436
Stephen <Stephen@Stephen-PC>
2016-10-17 20:32:25 -0400

    Basic outline for 10/18

--- Scrape.java
+++ Scrape.java
@@ -1,4 +1,17 @@
 class String Scrape {
-//test
+
+
+	public String webScrape(String input)
+	{
+		//Called from gui
+		
+		
+		//do some stuff and return a cool string to the GUI
+		
+		
+		return input;
+	}
 
 }
+
+
